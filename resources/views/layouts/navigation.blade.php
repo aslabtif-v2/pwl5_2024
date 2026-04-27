3e8317c1faf2dc7c32be47ff0edfbba7235e0626
unsur-labtif <labtif.unsur@gmail.com>
2026-04-27 14:44:45 +0700

    pertemuan 6

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('book')" :active="request()->routeIs('book')">
+                        {{ __('Daftar Buku') }}
+                    </x-nav-link>
                 </div>
 
                 
